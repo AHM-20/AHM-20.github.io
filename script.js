3108d33490454c77223ce7f4a92ecc0aa91a6f4b
Dehmy <abdulrahman.hussein.m@gmail.com>
2022-02-03 13:04:02 +0300

    new edit

--- script.js
+++ script.js
@@ -1,55 +1,52 @@
 AOS.init();
 
-$(document).ready(function() {
-	for (let index = 2; index < 8; index++) {
-		// $('.splide__list').append(
-		// 	`<li class="splide__slide"> <img src="/images/image\ \(${index}\).jpeg" alt="..."></li>`
-		// );
-	}
-	// $('.splide__slide').each().append(
-	// 	`<img src="/images/image\ \(${1}\).jpeg" alt="...">`
-	// );
-});
-const navBar = document.querySelector('#nav-bar');
-// navBar.style.background = '#fff'
+function getOffset(el) {
+	const rect = el.getBoundingClientRect();
+	return {
+		left: rect.left + window.scrollX,
+		top: rect.top + window.scrollY
+	};
+}
 
+const navBar = document.querySelector('#nav-bar');
+var navs = document.querySelectorAll('.nav-item');
 var prevScrollpos = window.pageYOffset;
-let isAnimationDone = false;
+
 window.onscroll = function() {
 	var currentScrollPos = window.pageYOffset;
-	// if (prevScrollpos > currentScrollPos) {
-	// 	navBar.style.top = '0';
-	// } else {
-	// 	navBar.style.top = '-50px';
-	// }
 
-	// if (currentScrollPos > 500 && !isAnimationDone) {
-	// 	isAnimationDone = true;
-	// 	anime({
-	// 		targets: '.project-section:nth-child(even),.project-section',
-	// 		translateX: [ 200, 0 ],
-	// 		// translateY: [200,0],
-	// 		// rotate: '1turn',
-	// 		// backgroundColor: '#FFF',
-	// 		delay: function(el, i, l) {
-	// 			return i * 200;
-	// 		}
-	// 		// duration: 1500
-	// 	});
-	// }
-
-	if (currentScrollPos > 500) {
-		// navBar.style.background = 'rgb(13, 124, 96)';
-		navBar.classList.add('nav-background');
+	if (currentScrollPos > 900 && currentScrollPos < 1900) {
+		var rmActive = document.getElementsByClassName('active')[0];
+		rmActive ? rmActive.classList.remove('active') : '';
+		var menuEl = document.querySelector(`.portfolio-menu`);
+		menuEl.classList.add('active');
+	} else if (currentScrollPos > 1900 && currentScrollPos < 2500) {
+		var rmActive = document.getElementsByClassName('active')[0];
+		rmActive ? rmActive.classList.remove('active') : '';
+		var menuEl = document.querySelector(`.Gallery-menu`);
+		menuEl.classList.add('active');
+	} else if (currentScrollPos > 2500) {
+		var rmActive = document.getElementsByClassName('active')[0];
+		rmActive ? rmActive.classList.remove('active') : '';
+		var menuEl = document.querySelector(`.contact-section-menu`);
+		menuEl.classList.add('active');
 	} else {
-		// navBar.style.background = 'transparent';
-		navBar.classList.remove('nav-background');
+		var rmActive = document.getElementsByClassName('active')[0];
+		rmActive ? rmActive.classList.remove('active') : '';
 	}
+
 	prevScrollpos = currentScrollPos;
 };
 
 const scrollNav = (section) => {
 	var element = document.getElementsByClassName(section)[0];
+	console.log(getOffset(element).top);
+
+	var menuEl = document.querySelector(`.${section}-menu`);
+	var rmActive = document.getElementsByClassName('active')[0];
+	rmActive ? rmActive.classList.remove('active') : '';
+
+	menuEl.classList.add('active');
 	element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
 };
 
@@ -57,11 +54,24 @@ const onContact = () => {
 	window.open('mailto:test@example.com?subject=subject&body=body');
 };
 
+const checkValue = () => {
+	var email = document.getElementById('email').value || '';
+	var subject = document.getElementById('subject').value || '';
+	var message = document.getElementById('message').value || '';
+
+	var btn = document.getElementById('sub');
+	if (email && subject && message) {
+		btn.classList.remove('disabled');
+	} else {
+		btn.classList.add('disabled');
+	}
+};
+
 $('#sub').click(function() {
 	var email = document.getElementById('email').value || '';
 	var subject = document.getElementById('subject').value || '';
 	var message = document.getElementById('message').value || '';
-	console.log(email, message, subject);
+
 	$(location).attr(
 		'href',
 		`mailto:${email}?subject=` + encodeURIComponent(subject) + '&body=' + encodeURIComponent(message)
@@ -74,7 +84,7 @@ document.addEventListener('DOMContentLoaded', function() {
 		autoWidth: true,
 		speed: 700,
 		autoplay: true,
-		focus  : 'center',
+		focus: 'center',
 		padding: '2rem'
 	});
 	splide.mount();
